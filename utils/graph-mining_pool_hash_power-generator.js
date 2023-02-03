319971d6140e9569a358257a4777d524d93d145f
BHD Dev <dev@bhd.one>
2023-02-03 16:16:42 +0800

    Graph `hash power of btc pools` is finished.

--- utils/graph-mining_pool_hash_power-generator.js
+++ utils/graph-mining_pool_hash_power-generator.js
@@ -1,4 +1,4 @@
-let POOLS = [
+const POOLS = [
     {
         name: "BTC.top",
         percent: 15.28,
@@ -57,10 +57,12 @@ let POOLS = [
     },
 ];
 
-const COLOR_NAMES = ["cyan", "magenta", "orange", "brown", "teal", "lime", "red", "green", "purple", "gray", "blue", "olive"];
+const COLOR_NAMES = ["cyan", "magenta", "orange", "brown", "teal", "lime", "purple", "gray", "blue", "olive"];
 const COLOR_OPACITIES = [60, 30];
 
 const RADIUS = 5;
+const INFO_LINE_LEN = 1;
+const INFO_LINE_HORZ = 0.5;
 
 const percentToAngle = (percent) => {
     return percent / 100 * 360;
@@ -103,9 +105,38 @@ const isColorTaken = (color, opacity) => {
     return false;
 }
 
+const FULL_BAND_WIDTH = (RADIUS + INFO_LINE_LEN + INFO_LINE_HORZ) * 2;
+const NUM_BANDS_1_LINE = 7;
+const BAND_WIDTH = FULL_BAND_WIDTH / NUM_BANDS_1_LINE;
+const BAND_HEIGHT = 0.7;
+const BAND_START_X = -(FULL_BAND_WIDTH / 2);
+const BAND_START_Y = BAND_START_X - (INFO_LINE_LEN + INFO_LINE_HORZ);
+const BAND_RECT_WIDTH = 0.3;
+
+const calcBandStartPosition = (idx) => {
+    const row = Math.floor(idx / NUM_BANDS_1_LINE);
+    const col = Math.floor(idx % NUM_BANDS_1_LINE);
+    const r = {
+        x: col * BAND_WIDTH + BAND_START_X,
+        y: row * BAND_HEIGHT + BAND_START_Y,
+    };
+    return r;
+}
+
+const genScriptForBand = (idx, color, opacity) => {
+    const r = calcBandStartPosition(idx);
+    return `\\filldraw[${color}!${opacity}] (${r.x},${r.y}) rectangle +(${BAND_RECT_WIDTH},${BAND_RECT_WIDTH});`;
+}
+
+const genScriptForBandText = (idx, name) => {
+    const r = calcBandStartPosition(idx);
+    return `\\draw (${r.x + BAND_RECT_WIDTH + 0.05},${r.y + BAND_RECT_WIDTH / 2}) node[anchor=west]{${name}};`;
+}
+
 let lastColor = "";
 let lastAngel = 0;
 let percentLeft = 100;
+let bandIdx = 0;
 console.log("\\begin{tikzpicture}");
 console.log("    \\tiny");
 for (const pool of POOLS) {
@@ -130,15 +161,21 @@ for (const pool of POOLS) {
     const infoAngel = lastAngel + thisAngel / 2;
     const infoStartX = calcPositionX(infoAngel, RADIUS);
     const infoStartY = calcPositionY(infoAngel, RADIUS);
-    const infoEndX = calcPositionX(infoAngel, RADIUS + 1);
-    const infoEndY = calcPositionY(infoAngel, RADIUS + 1);
+    const infoEndX = calcPositionX(infoAngel, RADIUS + INFO_LINE_LEN);
+    const infoEndY = calcPositionY(infoAngel, RADIUS + INFO_LINE_LEN);
     const infoToRight = infoAngel >= 0 && infoAngel <= 90 || infoAngel >= 270;
     const infoToTop = infoAngel < 180;
-    const infoToX = infoToRight ? infoEndX + 0.5 : infoEndX - 0.5;
+    const infoToX = infoToRight ? infoEndX + INFO_LINE_HORZ : infoEndX - INFO_LINE_HORZ;
     const infoText = `${pool.name}, $${pool.percent}\\%$`;
     const infoTextPos = infoToTop ? "above" : "below";
     console.log(`    \\draw (${infoStartX},${infoStartY}) -- (${infoEndX},${infoEndY}) -- (${infoToX},${infoEndY}) node[${infoTextPos}]{${infoText}};`);
+    // Band
+    const bandScriptForRect = genScriptForBand(bandIdx, pool.color, pool.opacity);
+    console.log(`    ${bandScriptForRect}`);
+    const bandScriptForName = genScriptForBandText(bandIdx, pool.name);
+    console.log(`    ${bandScriptForName}`);
     // Next
+    ++bandIdx;
     lastColor = color;
     lastAngel = angel;
     percentLeft -= pool.percent;
